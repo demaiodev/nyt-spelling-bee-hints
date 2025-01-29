4ac2c8761ef536f696e0e6d06d49d6dcd08719ed
demaiodev <fml1041@gmail.com>
2025-01-29 17:10:29 -0500

    add Button constructor, add toggle

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ const date = new Date().toLocaleDateString("en-ZA", {
 });
 const url = `https://www.nytimes.com/${date}/crosswords/spelling-bee-forum.html`;
 let displayObject = {};
+let hide = true;
 
 fetch(url)
   .then((response) => response.text())
   .then((text) => {
     processHints(text);
     renderHints();
-    renderCalculateButton();
+    target.insertAdjacentElement(
+      "afterend",
+      Button("Calculate", handleClick, true)
+    );
+    target.insertAdjacentElement("afterend", Button("Show/Hide", hideHints));
   });
 
 function processHints(text) {
@@ -40,12 +45,10 @@ function renderHints() {
   target.style.border = "1px solid #ccc";
   target.style.borderRadius = "5px";
   target.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.2)";
-
-  target.innerHTML = ""; 
-
+  target.innerHTML = "";
   for (const key in displayObject) {
     const circle = document.createElement("div");
-    circle.classList.add("hint-circle"); 
+    circle.classList.add("hint-circle");
     circle.style.display = "inline-block";
     circle.style.width = "30px";
     circle.style.height = "30px";
@@ -55,33 +58,36 @@ function renderHints() {
     circle.style.lineHeight = "30px";
     circle.style.boxShadow = "0 1px 2px rgba(0, 0, 0, 0.2)";
     circle.style.border = "1px solid #ccc";
-
     const hintSubstring = document.createElement("span");
     hintSubstring.textContent = key.toUpperCase();
     circle.appendChild(hintSubstring);
-
     const remainingCount = document.createElement("span");
     remainingCount.style.display = "block";
     remainingCount.textContent = displayObject[key];
     circle.appendChild(remainingCount);
-
-    target.appendChild(circle); 
+    target.appendChild(circle);
   }
 }
 
-function renderCalculateButton() {
-  const calculateButton = document.createElement("button");
-  calculateButton.innerText = "Calculate";
-  calculateButton.style.marginLeft = "1em";
-  calculateButton.style.padding = "8px 15px"; 
-  calculateButton.style.borderRadius = "3px"; 
-  calculateButton.style.backgroundColor = "#f9d924";
-  calculateButton.style.color = "black"; 
-  calculateButton.style.border = "none"; 
-  calculateButton.style.cursor = "pointer"; 
-  calculateButton.classList.add("calculate-button"); 
-  calculateButton.addEventListener("click", handleClick);
-  target.insertAdjacentElement("afterend", calculateButton);
+function Button(text, handlerFn, primary = false) {
+  const button = document.createElement("button");
+  button.innerText = text;
+  button.style.marginLeft = "1em";
+  button.style.padding = "8px 15px";
+  button.style.borderRadius = "3px";
+  button.style.color = "black";
+  button.style.border = "none";
+  button.style.cursor = "pointer";
+  if (primary) button.style.backgroundColor = "#f9d924";
+  button.classList.add("button");
+  button.addEventListener("click", handlerFn);
+  return button;
+}
+
+function hideHints() {
+  if (hide) target.style.opacity = 0;
+  else target.style.opacity = 1;
+  hide = !hide;
 }
 
 function handleClick() {
@@ -98,10 +104,10 @@ function handleClick() {
       displayObject[key] = hintsObject[key] - obj[key];
     }
   });
-  renderHints()
+  renderHints();
 }
 
-const style = document.createElement('style');
+const style = document.createElement("style");
 style.textContent = `
 .hint-circle {
   transition: background-color 0.3s ease-in-out; 
@@ -112,12 +118,16 @@ style.textContent = `
   cursor: pointer;
 }
 
-.calculate-button {
+.button {
   transition: all 0.15s ease-in-out; 
 }
 
-.calculate-button:hover {
+.button:hover {
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15); 
+}
+
+.pz-byline__text {
+  transition: opacity 0.3s ease-in-out;
 }
 `;
 document.head.appendChild(style);
